aa277c2becff906bece7da2b25b06d2aa656594f
travis <travis.x.wong@gmail.com>
2021-10-19 07:10:00 +0800

    vue-router初体验

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createWebHashHistory, createRouter} from 'vue-router'
+import Hi from './components/hi.vue'
 
-createApp(App).mount('#app')
+const history = createWebHashHistory()
+const router = createRouter({
+    history,
+    routes: [
+        {
+            path: '/', component: Hi
+        }
+    ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
