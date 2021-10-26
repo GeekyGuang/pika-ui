4e78901c809312b197caa6491db110ea1ab10d70
travis <travis.x.wong@gmail.com>
2021-10-27 06:28:56 +0800

    路由切换后隐藏菜单栏

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,7 @@
+import { router } from './router';
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
-import {createWebHashHistory, createRouter} from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-
-const history = createWebHashHistory()
-const router = createRouter({
-    history,
-    routes: [
-        {
-            path: '/', 
-            component: Home
-        },
-        {
-            path: '/doc', 
-            component: Doc,
-            children:[
-                {path: 'switch', component: SwitchDemo}
-            ]
-        }
-    ]
-})
 
 createApp(App)
   .use(router)
