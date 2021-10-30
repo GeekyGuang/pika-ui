0851fd8c36d7c8ede35ca3990b680d7cce260f4f
travis <travis.x.wong@gmail.com>
2021-10-30 22:20:43 +0800

    点击时切换

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,7 +1,22 @@
 <template>
-    <button><span></span></button>
+    <button @click="toggle" :class="{checked}"><span></span></button>
 </template>
 
+<script lang="ts">
+import { ref } from '@vue/reactivity'
+export default {
+  setup(){
+     const checked = ref(false)
+     const toggle = () => {
+         checked.value = !checked.value
+     }
+     return {
+         checked, toggle
+     }
+  }
+}
+
+</script>
 <style lang="scss" scoped>
   @use "sass:math";
   $h: 22px;
@@ -10,7 +25,7 @@
       height: $h;
       width: $h*2;
       border: none;
-      background: blue;
+      background: grey;
       border-radius: math.div($h,2);
       position: relative;
   }
@@ -23,7 +38,11 @@
       background: white;
       border-radius: math.div($h2,2);
   }
-  button:hover > span {
-      left: calc(100% - #{$h2} - 2px)
+  button.checked {
+      background: blue;
+
+      & > span {
+          left: calc(100% - #{$h2} - 2px)
+      }
   }
 </style>
