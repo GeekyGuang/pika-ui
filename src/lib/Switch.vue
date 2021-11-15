42b6142c6840abf32ed42aa1086071af34f98796
travis <travis.x.wong@gmail.com>
2021-11-15 15:37:57 +0800

    修改css, 点击时圆点变胖

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,7 +26,7 @@ export default {
       height: $h;
       width: $h*2;
       border: none;
-      background: grey;
+      background: #bfbfbf;
       border-radius: math.div($h,2);
       position: relative;
   }
@@ -38,16 +38,28 @@ export default {
       width: $h2;
       background: white;
       border-radius: math.div($h2,2);
-      transition: left 250ms;
+      transition: all 250ms;
   }
   button:focus {
       outline: none;
   }
   button.checked {
-      background: blue;
+      background: #1890ff;
 
       & > span {
           left: calc(100% - #{$h2} - 2px)
+      }
+  }
+  button:active {
+      > span {
+          width: $h2 + 4px;
+      }
+  }
+
+  button.checked:active {
+      > span {
+          width: $h2 + 4px;
+          margin-left: -4px;
       }
   }
 </style>
