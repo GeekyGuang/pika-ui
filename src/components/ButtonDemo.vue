7d41964497327a8a735137e2abfe5563613df3fa
travis <travis.x.wong@gmail.com>
2021-11-17 14:15:44 +0800

    修改按钮描述

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,44 +2,44 @@
     <h1>示例1</h1>
     <div>
         <Button>你好</Button>
-        <Button theme="button">你好</Button>
-        <Button theme="link">你好</Button>
-        <Button theme="text">你好</Button>
+        <Button theme="button">普通按钮</Button>
+        <Button theme="link">链接按钮</Button>
+        <Button theme="text">文字按钮</Button>
     </div>
     <h1>示例2</h1>
     <div>
         <div>
-            <Button size="big">大大大</Button>
-            <Button>普普通</Button>
+            <Button size="big">大按钮</Button>
+            <Button>普通按钮</Button>
             <Button size="small">小小小</Button>
         </div>
         <div>
-            <Button theme="link" size="big">大大大</Button>
-            <Button theme="link">普普通</Button>
-            <Button theme="link" size="small">小小小</Button>
+            <Button theme="link" size="big">大链接按钮</Button>
+            <Button theme="link">普通链接按钮</Button>
+            <Button theme="link" size="small">小链接按钮</Button>
         </div>
         <div>
-            <Button theme="text" size="big">大大大</Button>
-            <Button theme="text">普普通</Button>
-            <Button theme="text" size="small">小小小</Button>
+            <Button theme="text" size="big">大文字按钮</Button>
+            <Button theme="text">普通文字按钮</Button>
+            <Button theme="text" size="small">小文字按钮</Button>
         </div>
     </div>
     <h1>示例3</h1>
     <div>
         <div>
-            <Button level="main" size="big">大大大</Button>
-            <Button>普普通</Button>
-            <Button level="danger" size="small">小小小</Button>
+            <Button level="main" size="big">主要按钮</Button>
+            <Button>普通按钮</Button>
+            <Button level="danger" size="small">危险按钮</Button>
         </div>
         <div>
-            <Button theme="link" size="big" level="main">大大大</Button>
-            <Button theme="link">普普通</Button>
-            <Button theme="link" size="small" level="danger">小小小</Button>
+            <Button theme="link" size="big" level="main">主要链接按钮</Button>
+            <Button theme="link">普通链接按钮</Button>
+            <Button theme="link" size="small" level="danger">危险链接按钮</Button>
         </div>
         <div>
-            <Button theme="text" size="big" level="main">大大大</Button>
-            <Button theme="text">普普通</Button>
-            <Button theme="text" size="small" level="danger">小小小</Button>
+            <Button theme="text" size="big" level="main">主要文字按钮</Button>
+            <Button theme="text">普通文字按钮</Button>
+            <Button theme="text" size="small" level="danger">危险文字按钮</Button>
         </div>
     </div>
 </template>
