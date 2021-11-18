dd760d1cc2dfd24bcc8a48b37c7463dfdc7fa6ca
travis <travis.x.wong@gmail.com>
2021-11-18 16:18:27 +0800

    判断子标签的类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,22 @@
 <template>
     <div>Tabs组件</div>
+    <component :is="defaults[0]"/>
+    <component :is="defaults[1]"/>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+    setup(props,context){
+       const defaults = context.slots.default()
+       defaults.forEach(tag => {
+           if(tag.type !== Tab) {
+             throw new Error('Tabs的子标签必须为Tab')
+         }
+       })
+       return {
+           defaults
+       }
+    }
+}
+</script>
